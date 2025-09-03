2fc6660696e464b27173f6c5311f42097ef1480e
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2025-09-03 09:38:04 -0400

    New version

---
+++ package.py
@@ -0,0 +1,40 @@
+name = "ffmpeg"
+
+version = "7.1.1.hh.1.0.0"
+
+authors = [
+    "FFmpeg",
+]
+
+description = """Collection of libraries and tools for processing multimedia contents"""
+
+with scope("config") as c:
+    import os
+
+    c.release_packages_path = os.environ["HH_REZ_REPO_RELEASE_EXT"]
+
+requires = [
+    "nasm",
+    "yasm",
+    "x264",
+    "libpng",
+    "freetype",
+    "harfbuzz",
+]
+
+private_build_requires = []
+
+variants = []
+
+
+def commands():
+    env.REZ_FFMPEG_ROOT = "{root}"
+    env.FFMPEG_ROOT = "{root}"
+    env.FFMPEG_LOCATION = "{root}"
+
+    env.PATH.append("{root}/bin")
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+
+
+build_system = "cmake"
+uuid = "repository.FFmpeg"
