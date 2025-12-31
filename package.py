0b4780014e72c6f43b57d0c680a98253e78876bf
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2025-12-31 15:06:53 -0500

    rez files

---
+++ package.py
@@ -0,0 +1,43 @@
+name = "ffmpeg"
+
+version = "8.0.0.hh.1.0.0"
+
+authors = [
+    "FFmpeg",
+]
+
+description = """Collection of libraries and tools for processing multimedia contents"""
+
+with scope("config") as c:
+    import os
+
+    c.release_packages_path = os.environ["HH_REZ_REPO_RELEASE_EXT"]
+
+requires = [
+    "nasm",
+    "yasm",
+    "x264",
+    "libpng",
+    "freetype",
+    "harfbuzz",
+]
+
+private_build_requires = []
+
+variants = []
+
+
+def commands():
+    env.REZ_FFMPEG_ROOT = "{root}"
+    env.FFMPEG_ROOT = "{root}"
+    env.FFMPEG_LOCATION = "{root}"
+
+    # We want to make sure that REZ envs will use this ffmpeg
+    # and not any one installed in the system that may be available
+    # in /usr/bin. Use prepend here.
+    env.PATH.prepend("{root}/bin")
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+
+
+build_system = "cmake"
+uuid = "repository.FFmpeg"
